f66109c8ccd4f5dfad3394e41bf9db992098e12f
Jacob <70965524+jOaawd@users.noreply.github.com>
2025-02-20 09:58:14 -0800

    Update example.js

--- public/example.js
+++ public/example.js
@@ -25,3 +25,24 @@ document.getElementById("searchButton").onclick = function (event) {
 
     iframeWindow.src = __uv$config.prefix + __uv$config.encodeUrl(url);
 };
+
+
+
+document.getElementById("Google").onclick = function (event) {
+    event.preventDefault();
+
+    let url = document.getElementById("Google").value; // if no periods are detected in the input, search google instead
+    let searchUrl = "https://www.google.com/search?q=";
+
+    
+
+    if (!url.includes(".")) {
+        url = searchUrl + encodeURIComponent(url);
+    } else {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) { // if no http or https is detected, add https automatically
+            url = "https://" + url;
+        }
+    }
+
+    iframeWindow.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+};
